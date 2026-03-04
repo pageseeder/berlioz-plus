53c797521ec3bdc1257ae46cf222545eb06f7265
clauret <clauret@weborganic.com>
2026-03-04 17:13:51 +1100

    Chore: Enhance Javadoc comments in `OutputException` constructors and class-level documentation

--- src/main/java/org/pageseeder/berlioz/plus/exceptions/OutputException.java
+++ src/main/java/org/pageseeder/berlioz/plus/exceptions/OutputException.java
@@ -1,16 +1,17 @@
 /*
  * (c) Copyright LPCC Pty Ltd (Australia) 2016
  */
-
-/**
- *
- */
 package org.pageseeder.berlioz.plus.exceptions;
 
 import org.pageseeder.berlioz.content.ContentStatus;
 
 /**
  * Request exceptions caused while writing the output.
+ *
+ * @author Christophe Lauret
+ *
+ * @since 0.5.0
+ * @version 0.6.0
  */
 public class OutputException extends RequestException {
 
@@ -18,22 +19,28 @@ public class OutputException extends RequestException {
   private static final long serialVersionUID = 2026_03_04L;
 
   /**
-   * @param message
+   * Constructs a new {@code OutputException} with the specified detail message.
+   *
+   * @param message the detail message explaining the reason for the exception
    */
   public OutputException(String message) {
     super(ContentStatus.INTERNAL_SERVER_ERROR, message);
   }
 
   /**
-   * @param cause
+   * Constructs a new {@code OutputException} with the specified underlying cause.
+   *
+   * @param cause the underlying cause of this exception
    */
   public OutputException(Throwable cause) {
     super(ContentStatus.INTERNAL_SERVER_ERROR, cause);
   }
 
   /**
-   * @param message
-   * @param cause
+   * Constructs a new {@code OutputException} with the specified detail message and underlying cause.
+   *
+   * @param message the detail message explaining the reason for the exception
+   * @param cause the underlying cause of this exception
    */
   public OutputException(String message, Throwable cause) {
     super(ContentStatus.INTERNAL_SERVER_ERROR, message, cause);
